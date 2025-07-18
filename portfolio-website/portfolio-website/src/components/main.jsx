cd62644a3dea5442ed7163d3a7c297d46f09cb50
Andrew Friesen <99490619+Wrekky@users.noreply.github.com>
2025-07-17 20:59:12 -0400

    Removed mouse input as a uniform for the shader.

--- portfolio-website/portfolio-website/src/components/main.jsx
+++ portfolio-website/portfolio-website/src/components/main.jsx
@@ -1,5 +1,4 @@
 import { Canvas, useFrame } from "@react-three/fiber";
-import { Environment, OrbitControls } from "@react-three/drei"
 import vertexShader from "../shaders/test.vertex.glsl?raw"
 import fragmentShader from "../shaders/test.fragment.glsl?raw"
 import { useThree } from '@react-three/fiber'
@@ -7,7 +6,6 @@ import { useMemo } from 'react'
 import { Vector2 } from "three";
 import {useRef, useState} from 'react'
 
-//TODO: Mouse input recognized in shader, should be the light source.
 function FullscreenPlane() {
   const { camera, size } = useThree()
   const [width, height] = useMemo(() => {
@@ -17,27 +15,11 @@ function FullscreenPlane() {
     const width = height * (size.width / size.height)
     return [width, height]
   }, [camera, size])
-  const materialRef = useRef()
-
-  const uMouse = useRef(new Vector2(0.5,0.5))
-
-  useFrame(() => {
-    if (materialRef.current?.uniforms?.uMouse) {
-      materialRef.current.uniforms.uMouse.value.copy(uMouse.current)
-    }
   })
 
-  const onPointerMove = (event) => {
-    const x = event.clientX / size.width
-    const y = 1 - event.clientY / size.height
-    uMouse.current.set(x,y)
-  }
   return (
-    <mesh onPointerMove={onPointerMove}>
       <planeGeometry args={[width, height, 16, 16]} />
       <shaderMaterial 
-      ref={materialRef}
-      uniforms={{uMouse: uMouse.current}}
       vertexShader={vertexShader} 
       fragmentShader={fragmentShader} />
     </mesh>
--- portfolio-website/portfolio-website/src/shaders/test.fragment.glsl
+++ portfolio-website/portfolio-website/src/shaders/test.fragment.glsl
@@ -1,5 +1,3 @@
-varying vec2 pos;
-uniform vec2 uMouse;
 void main() {
     gl_FragColor = vec4(uMouse.x, 0.0, 1.0, 1.0);
 }
