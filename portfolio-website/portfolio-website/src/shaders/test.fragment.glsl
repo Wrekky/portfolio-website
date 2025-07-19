1a589dd475407ab2c24b249afdb5f6f0617db27c
Andrew Friesen <99490619+Wrekky@users.noreply.github.com>
2025-07-18 21:35:59 -0400

    Updated shader to light an area around the mouse.

--- portfolio-website/portfolio-website/src/shaders/test.fragment.glsl
+++ portfolio-website/portfolio-website/src/shaders/test.fragment.glsl
@@ -5,12 +5,14 @@ uniform vec2 mousePos;
 varying vec2 vUv;
 void main() {
     vec2 fragCoord = gl_FragCoord.xy;
-    
     vec3 col = 0.5 + 0.5*cos(iTime + vUv.xyx + vec3(0,2,4));
-
+    //Mouse pos is passed as 0-1 for both x and y, need to translate these to resolution based.
+    vec2 mousePosByRes = vec2(mousePos.x * iResolution.x, iResolution.y - (mousePos.y * iResolution.y));
+    //Calculating distance for lighting effect.
+    float coordDistance = sqrt((pow(fragCoord.x - mousePosByRes.x, 2.0)) + (pow(fragCoord.y - mousePosByRes.y,2.0)));
+    float coordDistanceNormalized = 1.0-(coordDistance / (iResolution.x + iResolution.y));
+    col = col * coordDistanceNormalized;
+    //Darkening everything a bit.
+    col = col * 0.8;
     gl_FragColor = vec4(col,1);
-    //drawing over color on mouse coords.
-    if (fragCoord.x > iResolution.x / 2.0) {
-        gl_FragColor = vec4(0,1,2,1);
-    }
 } 
