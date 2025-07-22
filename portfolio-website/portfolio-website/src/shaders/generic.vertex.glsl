f3ddef1a24b636d0bdc8d37aa3a3f59e8927c5dc
Andrew Friesen <99490619+Wrekky@users.noreply.github.com>
2025-07-21 22:13:45 -0400

    Simplified vertex shader.

--- portfolio-website/portfolio-website/src/shaders/generic.vertex.glsl
+++ portfolio-website/portfolio-website/src/shaders/generic.vertex.glsl
@@ -1,7 +1,6 @@
 varying vec2 vUv;
 void main() {
+    vec3 pos = position;
     vUv = uv;
-    vec4 t_position = vec4(position, 1.0);
-    t_position.xy = position.xy * 2.0 - 1.0;
-    gl_Position = t_position;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
 }
