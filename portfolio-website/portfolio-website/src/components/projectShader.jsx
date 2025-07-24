c4e9b537843d8ff236298c3e3ace259e1fd851ad
Andrew Friesen <99490619+Wrekky@users.noreply.github.com>
2025-07-23 23:46:38 -0400

    Tracked mouse position when on shader canvas. Calculated in from 0-1.

--- portfolio-website/portfolio-website/src/components/projectShader.jsx
+++ portfolio-website/portfolio-website/src/components/projectShader.jsx
@@ -4,13 +4,29 @@ import { useMemo } from 'react'
 import vertexShader from "../shaders/generic.vertex.glsl?raw"
 import fragmentShader from "../shaders/project.fragment.glsl?raw"
 import { TextureLoader } from "three";
+import { Vector2 } from "three";
+import {useRef} from 'react'
 function ShaderEffect() {
   const colorMap = useLoader(TextureLoader, '../src/resources/images/tempImage.png')
+  const meshRef = useRef();
+  const mousePos = useRef({value: new Vector2(0.0, 0.0)})
+  const width = 16;
+  const height = 9;
   return (
-    <mesh>
-      <planeGeometry args={[16, 9]} />
+    <mesh
+    ref={meshRef}
+    onClick={
+      (event) =>{
+        const worldPoint = event.point.clone();
+        //change to uv coordinates (0,1)
+        mousePos.current.value.set((worldPoint.x / width) + 0.5, (worldPoint.y / height) + 0.5)
+        //console.log(mousePos.current.value.x, "x", mousePos.current.value.y, "y");
+      }
+    }
+    >
+      <planeGeometry args={[width, height]} />
       <shaderMaterial 
-      uniforms={{testImage: {value: colorMap}}}
+      uniforms={{testImage: {value: colorMap}, mousePos: mousePos.current}}
       vertexShader={vertexShader} 
       fragmentShader={fragmentShader} />
     </mesh>
