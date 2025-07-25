ce50ef0b847fea2a6e4763571430e07f0a8d64a0
Andrew Friesen <andrewf5000@gmail.com>
2025-07-24 23:34:28 -0400

    Added mouse uniform, changed the image based on mouse position.

--- portfolio-website/portfolio-website/src/components/projectShader.jsx
+++ portfolio-website/portfolio-website/src/components/projectShader.jsx
@@ -25,10 +25,10 @@ function ShaderEffect() {
     }
     >
       <planeGeometry args={[width, height]} />
-      <shaderMaterial 
-      uniforms={{testImage: {value: colorMap}, mousePos: mousePos.current}}
-      vertexShader={vertexShader} 
-      fragmentShader={fragmentShader} />
+      <shaderMaterial
+        uniforms={{ testImage: { value: colorMap }, mousePos: { value: mousePos.current } }}
+        vertexShader={vertexShader}
+        fragmentShader={fragmentShader} />
     </mesh>
   )
 }
--- portfolio-website/portfolio-website/src/shaders/project.fragment.glsl
+++ portfolio-website/portfolio-website/src/shaders/project.fragment.glsl
@@ -1,5 +1,12 @@
 uniform sampler2D testImage;
+uniform vec2 mousePos;
+
 varying vec2 vUv;
 void main() {
-    gl_FragColor = texture2D(testImage, vUv);//vec4(0.6, 0.49, 0.86, 0.24);
+  vec4 colors = texture2D(testImage, vUv);
+  if(mousePos.x > 0.5) {
+    gl_FragColor = colors * 0.8;
+  } else {
+    gl_FragColor = colors * 1.2;
+  }
 }
