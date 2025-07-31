3d7e9879474579c86baa924f519a69dfb9bae29d
Andrew Friesen <andrewf5000@gmail.com>
2025-07-31 16:19:09 -0400

    added github pages yml file.

--- portfolio/astro.config.mjs
+++ portfolio/astro.config.mjs
@@ -7,6 +7,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://wrekky.github.io',
+  base: '/portfolio-website',
   vite: {
     plugins: [tailwindcss()]
   },
