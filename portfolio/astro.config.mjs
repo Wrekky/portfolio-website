2de057043f674169c2b3d76792c05783edbb4df2
Andrew Friesen <andrewf5000@gmail.com>
2025-07-31 18:16:40 -0400

    updated path in yml.
    added correct image path, fixed various paths across the website.

--- portfolio/astro.config.mjs
+++ portfolio/astro.config.mjs
@@ -8,7 +8,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://wrekky.github.io',
-  base: '/portfolio-website/portfolio/',
+  base: '/portfolio-website',
   vite: {
     plugins: [tailwindcss()]
   },
